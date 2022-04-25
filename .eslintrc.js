0b95079f1949626ca12c6a90bac8881813d8cb7f
home PC <femuto01040507@gmail.com>
2022-04-25 19:01:31 +0900

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+// https://eslint.org/docs/rules/
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: ['plugin:nuxt/recommended', 'prettier'],
+  parserOptions: {},
+  plugins: ['only-warn'],
+
+  rules: {
+    semi: ['warn', 'never'],
+    quotes: ['warn', 'single', { allowTemplateLiterals: true }],
+    'no-console': ['off'],
+    'comma-dangle': [
+      'warn',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'always-multiline',
+      },
+    ],
+    'no-unreachable': ['off'],
+  },
+}
---
+++ server-middleware/api/routerIndex.js
@@ -0,0 +1,38 @@
+import knex from '../database/main'
+import express from 'express'
+const router = express.Router()
+
+// router.post('/measuredData', async (req, res, next) => {
+//   try {
+//     await knex('measure_data').insert([{
+//       device_id: req.body.MAC,
+//       temperature: req.body.temperature_value,
+//       humidity: req.body.humidity_value,
+//       brightness_level: req.body.bright_value,
+//     }])
+//     //res.send('Hello ESP32 World!' + Math.floor(Date.now() / 1000))
+//     res.status(200).end()
+//   } catch (err) {
+//     res.status(400).json({ ok: false, statusText: err?.message || "Unknown Error" })
+//   }
+// })
+
+// router.post('/deviceName', async(req, res, next) => {
+//   try {
+//     await knex('devices').where('id', '=', req.body.MAC).update({name: req.body.Name, update_at: knex.fn.now()})
+//     res.status(200).end()
+//   } catch (err) {
+//     res.status(400).end()
+//   }
+// })
+
+router.get('/dbtest', async (req, res, next) => {
+  try {
+    const name = await knex('devices').select('*');
+    res.status(200).json({ ok: true, statusText: 'ok.', body: name })
+  } catch (err) {
+    res.status(400).json({ ok: false, statusText: err?.message || "Unknown Error" })
+  }
+})
+
+module.exports = router
