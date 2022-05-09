a304acb1aa14ac035567053fe47b4a482b676e0a
home PC <femuto01040507@gmail.com>
2022-05-10 00:31:47 +0900

    code format check

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,11 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card>
-        <h2>test1</h2>
-      </v-card>
-      <v-card>
-        <h3>test2</h3>
-      </v-card>
-    </v-col>
-  </v-row>
+  <v-container>
+    <v-row justify="center" align="center">
+      <v-col cols="12" sm="8" md="6">
+        <v-card> ここでは自分が飲んできたお酒を簡単に紹介することが出来たらなと思っています． </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
